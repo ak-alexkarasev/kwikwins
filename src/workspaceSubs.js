29a84504b064572caa160f7721cf2cef13258005
Alexandr Karasev <ak.alexkarasev@gmail.com>
2022-08-11 01:04:09 +0300

    workspacesubs finish

--- src/workspaceSubs.js
+++ src/workspaceSubs.js
@@ -2,6 +2,8 @@ import cn from 'classnames';
 import styles from './scss/main.module.scss';
 import logo from './img/header/logo.svg';
 import ava from './img/header/ava.png';
+import resize from './img/input/icon_textarea.svg';
+import file_add from './img/input/plus.svg';
 import { Input } from 'antd';
 function WorkspaceSubs() {
     return (
@@ -44,34 +46,68 @@ function WorkspaceSubs() {
 							<div className={cn(styles.title, styles.subs__title)}>
 								<h2>Add new product</h2>
 							</div>
-							<div className={styles.subs__content}>
-								<form action="#">
-									<div className={styles.subs__content_left}>
-										<div className={styles.subs__heading}><span>Product information</span></div>
-										<div className={styles.wrap__animated}>
-											<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
-											<label className={styles.wrap__animated_label} for="#input-product">Name of your product</label>
+							<form action="#">
+								<div className={styles.subs__heading}>Product information</div>
+								<div className={styles.subs__content}>
+										<div className={styles.subs__content_left}>
+											<div className={styles.subs__wrap}>
+												<div className={styles.wrap__animated}>
+													<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
+													<label className={styles.wrap__animated_label} for="#input-product">Name of your product</label>
+												</div>
+												<div className={styles.wrap__animated}>
+													<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
+													<label className={styles.wrap__animated_label} for="#input-product">Industry</label>
+												</div>
+												<div className={styles.wrap__animated}>
+													<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
+													<label className={styles.wrap__animated_label} for="#input-product">What type of product are you creating?</label>
+												</div>
+												<div className={styles.wrap__animated}>
+													<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
+													<label className={styles.wrap__animated_label} for="#input-product">What stage is the product in?</label>
+												</div>
+												<div className={styles.wrap__animated}>
+													<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
+													<label className={styles.wrap__animated_label} for="#input-product">What's your budget range?</label>
+												</div>
+											</div>
+											<div className={styles.subs__wrap}>
+												<div className={styles.subs__heading2}>Describe your product</div>
+												<div className={styles.default__textarea_wrap}>
+													<img src={resize} />
+													<textarea className={styles.default__textarea} placeholder='Enter a clear and concise description'></textarea>
+												</div>
+											</div>
+											
 										</div>
-										<div className={styles.wrap__animated}>
-											<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
-											<label className={styles.wrap__animated_label} for="#input-product">Name of your product</label>
+										<div className={styles.subs__content_right}>
+											<div className={styles.subs__wrap}>
+												<div className={styles.wrap__animated}>
+													<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
+													<label className={styles.wrap__animated_label} for="#input-product">Name of your product</label>
+												</div>
+												<div className={styles.wrap__animated}>
+													<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
+													<label className={styles.wrap__animated_label} for="#input-product">Industry</label>
+												</div>
+												<div className={styles.wrap__animated}>
+													<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
+													<label className={styles.wrap__animated_label} for="#input-product">What type of product are you creating?</label>
+												</div>
+											</div>
+											<div className={styles.subs__wrap}>
+												<div className={styles.subs__heading2}>Expectation</div>
+												<textarea className={styles.default__textarea} placeholder='Describe your expectations'></textarea>
+											</div>
+											<div className={styles.subs__add}>
+												<img src={file_add}/>
+												<p>Attach files that may be useful to familiarize yourself with the project</p>
+											</div>
+											<button className={styles.subs__btn} disabled>Save</button>
 										</div>
-										<div className={styles.wrap__animated}>
-											<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
-											<label className={styles.wrap__animated_label} for="#input-product">Name of your product</label>
-										</div>
-										<div className={styles.wrap__animated}>
-											<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
-											<label className={styles.wrap__animated_label} for="#input-product">Name of your product</label>
-										</div>
-										<div className={styles.wrap__animated}>
-											<Input className={styles.wrap__animated_input} required="true" type="text" placeholder=" "></Input>
-											<label className={styles.wrap__animated_label} for="#input-product">Name of your product</label>
-										</div>
-									</div>
-									
-								</form>
-							</div>
+								</div>
+							</form>
 						</div>
 					</div>
 				</section>
